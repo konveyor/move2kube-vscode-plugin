04edd72618bc32da95ed3341aa58fba969131607
Roshan Swain <swainroshan001@gmail.com>
2023-03-15 14:03:09 +0530

    Update the react modules to react 18
    This commit aupdates all the node packages to their latest version, majorly moving react and react dom to version 18. Additionally, changed from single root ele element to createRoot. Update the gitignore to exclude build files.
    
    Signed-off-by: Roshan Swain <swainroshan001@gmail.com>

--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+ReactDOMClient.createRoot(rootElement as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
