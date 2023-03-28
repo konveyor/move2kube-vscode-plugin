ee2b677ef7c32b001a2d0d9369a44eb8b75fa25a
Roshan Swain <swainroshan001@gmail.com>
2023-03-28 20:23:47 +0530

    feat: add select config file option for transform
    It lets the user select config file, user terminal for qa, or skip config file option using a quick pick and run the respective commands.
    
    Signed-off-by: Roshan Swain <swainroshan001@gmail.com>

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -50,3 +50,55 @@ export function showTimedInformationMessage(message: string, duration: number): 
     }
   });
 }
+
+export async function selectFile(title: string | ""): Promise<string | undefined> {
+  const options: vscode.OpenDialogOptions = {
+    canSelectFolders: false,
+    canSelectFiles: true,
+    canSelectMany: false,
+    openLabel: `Select File`,
+    title: title,
+  };
+  const result = await vscode.window.showOpenDialog(options);
+
+  if (result && result.length > 0) {
+    const filePath = result[0].fsPath;
+    return filePath;
+  }
+
+  // If the user does not select any folder, return undefined.
+  return undefined;
+}
+
+export async function getUserConfigOption(): Promise<string[]> {
+  const selectFileOption = "Select File";
+  const enterInTerminalOption = "Answer config in terminal";
+  const skipOption = "Skip : Uses default";
+
+  const userChoice = await vscode.window.showQuickPick(
+    [selectFileOption, enterInTerminalOption, skipOption],
+    { placeHolder: "Select how you would like to add config details" }
+  );
+
+  let args: string[] = [];
+
+  switch (userChoice) {
+    case selectFileOption:
+      const fileUri = await selectFile("Select <config>.yaml file");
+      if (fileUri) {
+        args.push("-f", fileUri);
+      }
+      break;
+    case enterInTerminalOption:
+      // Run the command with default arguments.
+      break;
+    case skipOption:
+      args.push("--qa-skip");
+      break;
+    default:
+      // Running the command with default arguments. Same as "enterInTerminalOption" as it gives the user to answer the questions and modify the config.
+      break;
+  }
+
+  return args;
+}
